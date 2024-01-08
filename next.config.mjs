48765998ab6be0f11d18ecd18356fae9a0736866
Almaju <almaju@live.fr>
2024-01-08 19:51:39 +0100

    remark-code-import

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,20 @@
 import nextra from "nextra"
+import codeImport from "remark-code-import"
+import path from "node:path"
 
 const withNextra = nextra({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.tsx",
-  defaultShowCopyCode: true
+  defaultShowCopyCode: true,
+  mdxOptions: {
+    remarkPlugins: [
+      () =>
+        codeImport({
+          removeRedundantIndentations: true,
+          rootDir: path.join(process.cwd(), "todo")
+        })
+    ]
+  }
 })
 
 export default withNextra()
