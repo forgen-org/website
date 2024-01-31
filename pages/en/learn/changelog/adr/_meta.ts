502007623ba89a6493c5847fce033a4b72ef008a
Almaju <almaju@live.fr>
2024-01-31 13:46:01 +0100

    Add event producer ADR

--- pages/en/learn/changelog/adr/_meta.ts
+++ pages/en/learn/changelog/adr/_meta.ts
@@ -1,4 +1,5 @@
 export default {
+  "2024_01_30_event_producer": "2024-01-30 Event Producer",
   "2024_01_05_domain_logic_ownership": "2024-01-05 Domain Logic Ownership",
   "2023_12_27_how_to_build_projections": "2023-12-27 How to build Projections",
   "2023_12_17_splitting_infrastructure_in_two":
